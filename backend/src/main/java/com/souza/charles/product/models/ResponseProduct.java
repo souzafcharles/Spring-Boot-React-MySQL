d3cf708e000549936f492bd1ff49dd100a02aa4c
souzafcharles <souzach@outlook.com>
2025-03-14 16:58:53 -0300

    feat: request validation model

--- backend/src/main/java/com/souza/charles/product/models/ResponseProduct.java
+++ backend/src/main/java/com/souza/charles/product/models/ResponseProduct.java
@@ -1,4 +1,16 @@
 package com.souza.charles.product.models;
 
-public class ResponseProduct {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@Getter
+@Setter
+public class ResponseProduct implements Serializable {
+
+    private String message;
+
 }
