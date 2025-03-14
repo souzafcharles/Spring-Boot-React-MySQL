d1d8ed8b07108d2adcbde5c58ad65bc72085dd1e
souzafcharles <souzach@outlook.com>
2025-03-14 16:56:19 -0300

    feat: controller configuration

--- backend/src/main/java/com/souza/charles/product/controllers/ProductController.java
+++ backend/src/main/java/com/souza/charles/product/controllers/ProductController.java
@@ -1,4 +1,13 @@
 package com.souza.charles.product.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+    @GetMapping("/")
+    public String route() {
+        return "Product API is working!";
+    }
 }
